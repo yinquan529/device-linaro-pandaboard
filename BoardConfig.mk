f672a1b90ff0d75643237ac4a43c912e4adaf3b4
Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>
2011-10-20 00:13:53 +0159

    pandaboard: Remove duplicate TARGET_CPU_SMP definition
    TARGET_CPU_SMP := true was set twice. While this doesn't hurt, it's
    superfluous and might do unexpected things in test builds that try
    to disable TARGET_CPU_SMP...
    
    Signed-off-by: Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,7 +6,6 @@
 TARGET_BOARD_PLATFORM := omap4
 TARGET_NO_BOOTLOADER := true # Uses u-boot instead 
 TARGET_NO_KERNEL := false
-TARGET_CPU_SMP := true
 KERNEL_CONFIG := android_omap4_defconfig
 TARGET_USE_UBOOT := true
 UBOOT_CONFIG := omap4_panda_config
