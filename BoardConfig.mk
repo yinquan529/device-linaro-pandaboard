57d71c8b91024509c136312c7a72aab35b2bb5b3
HariGopal <harigopal.gollamudi@linaro.org>
2012-12-10 06:59:01 +0000

    Wifi enabling for Pandaboard4460
    Change-Id: I2dff9e83f3f2685630810036b74fc3aad6eec10d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,7 +31,9 @@ USE_CAMERA_STUB := true
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
 
-BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+# Enabling Wifi on Pandaboard 4460
+#BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+BOARD_WPA_SUPPLICANT_DRIVER      := WEXT
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := private_lib_driver_cmd
 # Below condition allows us to check if the image is being built for
